1a64d7f4e6f459e898e46ab9ac5d5052b89d2733
CheolHee Kim <windowlife92@gmail.com>
2023-03-08 01:22:19 +0900

    230308_0122 commit

--- shop_230208/src/mvc/controller/BoardController.java
+++ shop_230208/src/mvc/controller/BoardController.java
@@ -4,14 +4,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import java.util.List;
 import java.util.ArrayList;
